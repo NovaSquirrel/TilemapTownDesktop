34686efbcc527eb353a314ba9dc11700951ac240
NovaSquirrel <bushytail@novasquirrel.com>
2026-01-02 10:27:57 -0500

    Commit a license

---
+++ README.md
@@ -0,0 +1,2 @@
+# TilemapTownDesktop
+A Tilemap Town desktop client, using Qt6. Treat it as a proof-of-concept for now.
--- tilemaptownmapview.cpp
+++ tilemaptownmapview.cpp
@@ -1,6 +1,21 @@
-// Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-
+/*
+ * Tilemap Town desktop client
+ *
+ * Copyright (C) 2025 NovaSquirrel
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "tilemaptownmapview.h"
 #include "town.h"
 
